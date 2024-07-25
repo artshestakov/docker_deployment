f4867c465b9eea95a32792d14edf1eadcf77beed
Shestakov Artem <art.shestakov@icloud.com>
2024-07-25 18:56:45 +0300

    Переписан скирпт пересборки контейнеров на docker-compose. Удалены лишние/устаревшие скрипты. Добавлен контейнер для волчьего бота.

--- BuildContainer.sh
+++ BuildContainer.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
 
 CONTAINER_NAME=$1
-DOCKER_FILE_PATH=$2
-PARENT_DIR="$(dirname "$DOCKER_FILE_PATH")"
 
-if [[ -z $CONTAINER_NAME || -z $DOCKER_FILE_PATH ]]; then
-  echo 'Invalid arguments!'
-  echo 'Example: ./BuildContainer.sh [container_name] [path_to_dockerfile]'
-  exit 1
+if [[ -z $CONTAINER_NAME ]]; then
+    echo "Argument is empty!"
+    echo "Example: ./BuildContainer.sh [container_name]"
+    exit 1
 fi
 
-# Переходим в директорию скрипта
-cd "$(dirname "$0")"
-
-# Если контейнер существует - останавливаем и удаляем
-if [ "$(sudo docker ps --all --quiet --filter="name=$CONTAINER_NAME")" ]; then
-
-    echo Container "$CONTAINER_NAME" is exists. It will be deleted...
-    sleep 1
-
-    echo Stopping container...
-    sudo docker container stop $(sudo docker ps --all --quiet --filter="name=$CONTAINER_NAME")
-
-    echo Deleting container...
-    sudo docker container rm --force $(sudo docker ps --all --quiet --filter="name=$CONTAINER_NAME")
-
+# Проверим, есть ли вообще такой контейнер
+sudo docker-compose ps $CONTAINER_NAME &> /dev/null
+if [ "$(echo $?)" -ne "0" ]; then
+    echo "Such container does not exist! Check your docker-compose.yml"
+    exit 1
 fi
 
-# Если образ существует - удаляем
-if [ "$(sudo docker images --quiet $CONTAINER_NAME)" ]; then
-
-    echo Deleting image...
-    sudo docker image rm --force $(sudo docker images --quiet $CONTAINER_NAME)
-
+# Нашли контейнер - попробуем выполнить его остановку и удаление
+sudo docker-compose rm --force --stop $CONTAINER_NAME
+if [ "$(echo $?)" -ne "0" ]; then
+    exit 1
 fi
 
-# Собираем
-sudo docker build \
-  --no-cache \
-  --progress=plain \
-  --tag $CONTAINER_NAME \
-  --file $DOCKER_FILE_PATH \
-  $PARENT_DIR
+# Выполняем сборку контейнера
+sudo docker-compose build --parallel --progress plain $CONTAINER_NAME
+if [ "$(echo $?)" -ne "0" ]; then
+    exit 1
+fi
 
+# И запускаем
+sudo docker-compose up --detach $CONTAINER_NAME
+if [ "$(echo $?)" -ne "0" ]; then
+    exit 1
+fi
+
+# И покажем что там получилось
+sudo docker-compose ps $CONTAINER_NAME
+
+echo
+echo -e "\033[1;32mThe container '$CONTAINER_NAME' started successfully\033[0m"
