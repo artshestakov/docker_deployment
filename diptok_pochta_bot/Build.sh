3c122c7e25213e06a0fbe9cc883c6e44a495c2be
Shestakov Artem <art.shestakov@icloud.com>
2024-07-25 22:41:29 +0300

    Перевод на docker-compose diptok_pochta_bot

--- diptok_pochta_bot/Build.sh
+++ diptok_pochta_bot/Build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-CONTAINER_NAME=diptok_pochta_bot
-
 # Переходим в директорию скрипта
 cd "$(dirname "$0")"
 
@@ -18,38 +16,9 @@ cd rep
 # Собираем
 ./Build.sh
 
-# Забираем артефакты и чистим за собой
-cp build/bin/diptok_pochta_bot ../
-cp diptok_pochta_bot/bot.lang /opt/diptok_pochta_bot
+# Забираем бинарники и чистим за собой
+mv Bin/Release/diptok_pochta_bot /opt/diptok_pochta_bot
 cd ..
 
-# Останавливаем контейнер и делаем чистку
-sudo docker container stop $(sudo docker ps --all --quiet --filter="name=$CONTAINER_NAME")
-sudo docker image rm --force $(sudo docker images --quiet $CONTAINER_NAME)
-sudo docker container rm --force $(sudo docker ps --all --quiet --filter="name=$CONTAINER_NAME")
-
-# Собираем
-sudo docker build \
---no-cache \
---progress=plain \
---tag $CONTAINER_NAME .
-
-# Запускаем
-sudo docker run \
---detach \
---restart=always \
---name $CONTAINER_NAME \
---volume /opt/diptok_pochta_bot/Logs:/opt/Logs \
---volume /opt/diptok_pochta_bot/Users:/opt/Users \
---volume /opt/diptok_pochta_bot/config.ini:/opt/config.ini \
---volume /opt/diptok_pochta_bot/bot.lang:/opt/bot.lang \
-$CONTAINER_NAME
-
-# Чистим за собой артефакты
-rm diptok_pochta_bot
 rm -rf rep
 
-sleep 1
-
-# И посмотрим, что там получилось
-sudo docker ps --all
--- diptok_pochta_bot/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/opt/diptok_pochta_bot
