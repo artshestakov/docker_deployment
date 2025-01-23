057eeb643bf10d7b1a408f1727924cd17d141199
Shestakov Artem <art.shestakov@icloud.com>
2025-01-24 01:06:33 +0300

    Добавлены скрипты для получения использования процессора и памяти докер-контейнеров

---
+++ zabbix_scripts/docker_cpu_usage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CONTAINER_NAME=$1
+
+# Если имя контейнера не передали - ругаемся и выходим
+if [ -z "$CONTAINER_NAME" ]
+then
+    echo "ERROR: container name not specified."
+    exit 1
+fi
+
+# Вытаскиваем потребление процессора формате 15.4%
+OUTPUT_STATS=$(sudo docker stats --all --no-stream --format "{{.CPUPerc}}" $CONTAINER_NAME)
+
+# Убедимся, что Докер отработал без ошибок
+if [ $? -ne 0 ]
+then
+    # При ошибке молча отдаём ноль
+    echo 0
+
+    exit 1
+fi
+
+# Удаляем символ процента и печатаем в консоль
+echo "${OUTPUT_STATS//%}"
+
