504ddacae705d59d71fc339ead6d15a0dd1ecc35
Shestakov Artem <art.shestakov@icloud.com>
2025-04-25 22:25:16 +0300

    Поправлен скрипт мониторинга потребления памяти

--- zabbix_scripts/memory_service.sh
+++ zabbix_scripts/memory_service.sh
@@ -10,24 +10,14 @@ if [[ -z $SERVICE_NAME ]]; then
     exit 1
 fi
 
-OUTPUT=$(service $SERVICE_NAME status | grep Memory | grep -Po '\d+(,|.|null)\d+\w')
+# Вытаскиваем PID процесса
+PID=$(pgrep "$SERVICE_NAME")
 
-# Вытаскиваем число
-MEMORY=$(echo $OUTPUT | grep -Po '^\d+(.|,)\d+')
+# Вытаскиваем фактическое потребление паммяти процессом
+MEMORY=$(cat /proc/$PID/status | grep -E "VmRSS" | grep -Po "\d+")
 
-# Вытаскиваем единицу измерения: K (kb), M (mb) или G (gb)
-UNIT=$(echo $OUTPUT | grep -Po '([A-Z]+|[a-z]+)+')
-
-# И приводим к нижнему регистру, для безопасного сравнения дальше
-UNIT=$(echo "${UNIT,,}")
-
-# Если значение пришло в килобайтах - то округляем до 1 мегабайта, чтобы не заморачиваться
-if [[ "$UNIT" == "k" ]]
-then
-    MEMORY=1
-elif [[ "$UNIT" == "g" ]] # Значение в гигабайтах - переводим в мегабайты
-then
-    MEMORY=$(echo "$MEMORY * 1024" | bc)
-fi
+# Переводим в мегабайты, т.к. VmRSS измеряется в килобайтах
+MEMORY=$(echo "$MEMORY / 1024" | bc)
 
 echo $MEMORY
+
